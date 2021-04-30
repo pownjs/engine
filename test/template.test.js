a7d64e7903443bebc2e0a831f2687cc5b843742f
Petko D. Petkov <pdp@websecurify.com>
2021-04-30 11:14:45 +0000

    Added basic tests.

--- test/template.test.js
+++ test/template.test.js
@@ -1,3 +1,14 @@
-describe('#template', () => {
-    // TODO: add code here
+const assert = require('assert')
+
+const { Template } = require('../lib/template')
+
+describe('template', () => {
+    describe('Template', () => {
+        it('#test', async() => {
+            const t = new Template()
+
+            assert(await t.test({ b64: Buffer.from('test').toString('base64') }, 'test'))
+            assert(!await t.test({ b64: Buffer.from('test').toString('base64') }, 'tset'))
+        })
+    })
 })
